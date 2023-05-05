27a4642f585faef10dd0881688a908ed033691f6
macky-droid <78133874+macky-droid@users.noreply.github.com>
2023-05-05 12:46:24 +0800

    Initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function() {
+  const ctaButton = document.querySelector('.cta-button');
+
+  ctaButton.addEventListener('click', function() {
+      alert('Welcome to the Forest Visualization Experience!');
+  });
+});
+
